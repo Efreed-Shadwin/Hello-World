98bb265d46dccaa2831b143996aea5fb28f01321
Efreed-Shadwin <146854297+Efreed-Shadwin@users.noreply.github.com>
2023-10-27 14:28:40 -0500

    fixed the show major if else

--- show_major.js
+++ show_major.js
@@ -1,25 +1,25 @@
 "use strict"
-var studentMajor = "BIOL" 
+var studentMajor = "CSCI" 
 var studentName = "Sidney"
 var classification
 var location 
-if (studentMajor= "BIOL"){
+if (studentMajor== "BIOL"){
     classification = "Biology"
     location = "Science Bldg, Room 310"
 }
-else if (studentMajor = "CSCI"){
+else if (studentMajor == "CSCI"){
     classification = "Computer Science"
     location = "Sheppard Hall, Room 314"
 }
-else if (studentMajor = "ENG"){
+else if (studentMajor =="ENG"){
     classification = "English"
     location = "Kerr Hall, Room 201"
 }
-else if (studentMajor = "HIST"){
+else if (studentMajor == "HIST"){
     classification = "History"
     location = "Kerr Hall, Room 114"
 }
-else if (studentMajor = "MKT"){
+else if (studentMajor == "MKT"){
     classification = "Marketing"
     location = "Westly Hall, Room 310"
 }
